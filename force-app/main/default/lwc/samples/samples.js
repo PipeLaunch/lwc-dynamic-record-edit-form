3c5e110aec523215ca7d3b60bc4cb27673922e48
Samuel Carreira <samuel@pipelaunch.com>
2023-12-02 23:52:26 +0000

    customize tabs, prepare for hidden fields sample

--- force-app/main/default/lwc/samples/samples.js
+++ force-app/main/default/lwc/samples/samples.js
@@ -2,7 +2,7 @@
  * @description       : Sample usage of the custom component
  * @group             : Generic Components
  * @author            : samuel@pipelaunch.com
- * @last modified on  : 2023-07-29
+ * @last modified on  : 2023-12-02
  * @last modified by  : samuel@pipelaunch.com
  **/
 import { LightningElement } from "lwc";
@@ -21,6 +21,9 @@ export default class Samples extends LightningElement {
     },
     Name: {
       hidden: true,
+    },
+    Field_Not_Visible__c: {
+      value: "test",
     },
   };
 
